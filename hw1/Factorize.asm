29b4a4b3ee7378b58ffc0b1063cb51389491ca3f
Yasir Fidan <yasirfidan@windowslive.com>
2021-03-28 18:23:35 +0300

    add Factorize.asm

---
+++ hw1/Factorize.asm
@@ -0,0 +1,58 @@
+.data
+	msg: .asciiz "Enter a number for finding its factors: "
+	comma: .asciiz ","
+	header: .asciiz "factors of "
+	is: .asciiz " is "
+
+.text
+	
+
+findFactors:
+	addi $t1,$0,1
+loop:
+	div $t0,$t1
+	mfhi $t3 # get mod result to t3
+	beq $t3,$0,FactorFounded
+cont:
+	addi $t1,$t1,1
+	bgt $t1,$t0,exit
+	j loop
+
+FactorFounded:
+	# print #t1
+	li $v0, 1
+	add $a0,$t1,$0
+	syscall
+
+	li $v0,4
+	la $a0,comma
+	syscall
+	j cont
+
+exit:
+	li $v0,10
+	syscall
+
+main:
+	li $v0,4
+	la $a0,msg
+	syscall
+
+	li $v0,5
+	syscall
+	move $t0,$v0
+
+	li $v0,4
+	la $a0,header
+	syscall
+
+	li $v0, 1
+	add $a0,$t0,$0
+	syscall
+
+	li $v0,4
+	la $a0,is
+	syscall
+
+	
+	jal findFactors
